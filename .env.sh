7536a7400af348054ba8aae7f9f575d4a81b177b
jeffreywescott <jeffrey@learnersguild.org>
2018-05-11 13:30:45 -0700

    fix autocomplete and autocorrect for zsh

--- .env.sh
+++ .env.sh
@@ -4,7 +4,6 @@ export PATH="\
 $HOME/bin:\
 /usr/local/sbin:\
 /usr/local/share/npm/bin:\
-./node_modules/.bin:\
 /usr/local/heroku/bin:\
 $PATH:\
 /Applications/Visual Studio Code.app/Contents/Resources/app/bin\
@@ -12,3 +11,15 @@ $PATH:\
 
 export NPM_AUTH_TOKEN=$(cat $HOME/.npmrc | grep _authToken | cut -d '=' -f2)
 export NODE_ENV="development"
+
+# Adds node_modules/.bin to the PATH
+npm_chpwd_hook() {
+    if [ -n "${PRENPMPATH+x}" ]; then
+        PATH=$PRENPMPATH
+        unset PRENPMPATH
+    fi
+    if [ -f package.json ]; then
+        PRENPMPATH=$PATH
+        PATH=$(npm bin):$PATH
+    fi
+}
